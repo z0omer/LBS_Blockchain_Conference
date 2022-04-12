a43e079a51e2923d873bcf42807831ea6cc74cb7
dariasaji <dariasaji97@gmail.com>
2022-04-12 15:02:01 +0100

    Build error resolve

--- src/components/alumni.jsx
+++ src/components/alumni.jsx
@@ -11,7 +11,7 @@ export const Alumni = (props) => {
                 <div key={`${d.name}-${i}`} className='col-md-4'>
                   <div className='alumni'>
                     <div className='alumni-image'>
-                      <img src={d.img} alt="alumni image"/>
+                      <img src={d.img} alt="alumni details"/>
                     </div>
                     <div className='alumni-content'>
                     <div className='alumni-meta'> {d.name} </div>
